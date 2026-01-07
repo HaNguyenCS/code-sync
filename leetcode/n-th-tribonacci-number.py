45409d64a8f85c924029246188f6a31e308b35bd
Thanh Ha Nguyen <97636863+HaNguyenCS@users.noreply.github.com>
2026-01-07 12:51:58 -0500

    Add solution for n-th-tribonacci-number

---
+++ leetcode/n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int, memo = {}) -> int:
+
+        if n in memo:
+            return memo[n]
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+
+        memo[n] = self.tribonacci(n-1, memo) + self.tribonacci(n-2, memo) + self.tribonacci(n-3, memo)
+        return memo[n]
